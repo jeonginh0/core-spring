8b703bbc74317266eca7debec4ecb8f7ef5dbea0
jeonginh0 <stitch010913@gmail.com>
2024-05-02 23:45:10 +0900

    스프링 핵심 학습 Chapter6 ++ 컴포넌트 스캔과 자동 의존관계 주입 (1)

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository; // 추상화에만 의존 DIP를 지킨다.
 
+    @Autowired // ac.getbean(MemberRepository.class)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
